40efba268e283efb29fa332b1ea20188698c8427
ale <42730179@fi.unju.edu.ar>
2026-01-26 00:07:09 -0300

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -141,7 +141,7 @@ export default function Footer() {
               <li className="flex items-start gap-3 text-foreground/60">
                 <FaMapMarkerAlt className="mt-1 text-primary shrink-0" />
                 <span>
-                  {personalData?.address || "Dirección..."}
+                 
                   <br />
                   {personalData?.address ? `${personalData.address}` : ""} 
                 </span>
