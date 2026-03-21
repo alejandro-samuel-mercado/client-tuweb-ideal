5ead84211a7737c1019223ecce9a54b83f9325e8
ale <42730179@fi.unju.edu.ar>
2026-03-20 22:46:35 -0300

    fix plans and content

--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -74,7 +74,7 @@ export default function Plans() {
                     initial="hidden"
                     whileInView="visible"
                     viewport={{ once: true }}
-                    className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-20 overflow-x-auto"
+                    className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-20"
                 >
                     {plans.filter(plan => locale === 'en' ? !!plan.description_en : !!plan.description).map((plan) => (
                         <motion.div
